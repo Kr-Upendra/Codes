87ad6d6eaf181b89d6f0703e72748eedcfab81cf
Upendra K <toxic74412@gmail.com>
2022-07-09 22:30:56 +0530

    Complete all different type of methods to find Fibonacci number

--- Fibonacci.java
+++ Fibonacci.java
@@ -17,13 +17,14 @@ public class Fibonacci {
 
     // using Recursion 
     private static int fibRecursion(int n) {
-        if (n == 0 || n == 1) return 1;
+        if (n == 0 || n == 1) return n;
         return fibRecursion(n-1) + fibRecursion(n-2);
     }
 
+    // using Memoization 
     private static int fibDPMemo(int n, int[] dp) {
         if (n <= 1) {
-            dp[n] = 1;
+            dp[n] = n;
             return dp[n];
         }
         if (dp[n] == -1) return dp[n];
@@ -31,10 +32,27 @@ public class Fibonacci {
         return dp[n];
     }
 
+    // using tabulation 
+    private static int fibTabulation(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
-        var n = sc.nextInt();
+        var n = 10 ;//sc.nextInt();
+        int[] dp = new int[n+1];
         var answer = fib1(n);
         var answer2 = fibRecursion(n);
+        var answer3 = fibDPMemo(n, dp);
+        var answer4 = fibTabulation(n);
         System.out.println(answer);
+        System.out.println(answer2);
+        System.out.println(answer3);
+        System.out.println(answer4);
     }
 }
